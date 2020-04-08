c9319b601f618a07edc2ca048e9a84877e614d67
Rongjian Zhang <pd4d10@gmail.com>
2020-04-08 15:56:45 +0800

    chore: default outline icon

--- scripts/icon.js
+++ scripts/icon.js
@@ -3,16 +3,12 @@ const path = require("path");
 const { execSync } = require("child_process");
 
 // Icons: https://ionicons.com/
-const flash = fs.readFileSync(path.join(__dirname, "flash.svg"), "utf8");
-const flashOutline = fs.readFileSync(
-  path.join(__dirname, "flash-outline.svg"),
-  "utf8"
-);
-
-const icon = flash.replace(
-  "viewBox='0 0 512 512'",
-  "viewBox='-32 -32 576 576'"
-);
+const icon = fs
+  .readFileSync(path.join(__dirname, "flash.svg"), "utf8")
+  .replace("viewBox='0 0 512 512'", "viewBox='-32 -32 576 576'");
+const iconOutline = fs
+  .readFileSync(path.join(__dirname, "flash-outline.svg"), "utf8")
+  .replace("viewBox='0 0 512 512'", "viewBox='-32 -32 576 576'");
 
 // Colors: https://material.io/resources/color/#!/?view.left=0&view.right=0
 [
@@ -27,6 +23,19 @@ const icon = flash.replace(
     icon
       .replace("width='512' height='512'", `width='${size}' height='${size}'`)
       .replace("<path", `<path fill="${color}"`)
+  );
+  execSync(`rm -rf ${name}.png && svg2png ${name}.svg`, {
+    cwd: path.join(__dirname, "../icons"),
+  });
+});
+
+// default
+[["default", "#bdbdbd", 38]].forEach(([name, color, size]) => {
+  fs.writeFileSync(
+    path.join(__dirname, "../icons", `${name}.svg`),
+    iconOutline
+      .replace("width='512' height='512'", `width='${size}' height='${size}'`)
+      .replace("stroke:#000;", `stroke:${color};`)
   );
   execSync(`rm -rf ${name}.png && svg2png ${name}.svg`, {
     cwd: path.join(__dirname, "../icons"),
