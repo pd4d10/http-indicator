4f9deead77121b8d20e6503dc31efbe6590933c4
UnknownErrror <herobrine135111@gmail.com>
2021-11-30 20:43:09 +0300

    Better detection for protocols
    Added SPDY, HTTP/1.1, HTTP/1.0 detection support
    Modified existing detection

--- src/background.js
+++ src/background.js
@@ -2,25 +2,32 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   const tabId = sender.tab.id;
   // console.log(message, tabId)
   let title = message;
-  let icon;
+  let icon = "h1";
 
   // h2 and hq is introduced from this post:
   // https://developers.google.com/web/updates/2017/12/chrome-loadtimes-deprecated
   // But the latest Chrome (68) seems use the value like this:
   // http/2+quic/43
   // Handle both cases for compatible
-  if (message === "h2") {
-    title = "HTTP/2";
-    icon = "h2";
-  } else if (message === "hq") {
+  if (message === "hq") {
     title = "HTTP/2 + QUIC";
     icon = "hq";
   } else if (message.startsWith("h3")) {
+    title = "HTTP/3";
     icon = "h3";
+  } else if (message.startsWith("h2")) { // "h2", "h2c"
+    title = "HTTP/2";
+    icon = "h2";
   } else if (message.includes("quic")) {
+    title = "QUIC";
     icon = "hq";
-  } else {
-    icon = "h1";
+  } else if (message.startsWith("spdy")) { // "spdy/1", "spdy/2", "spdy/3"
+    title = "SPDY";
+    icon = "spdy";
+  } else if (message === "http/1.1") {
+    title = "HTTP/1.1";
+  } else if (message === "http/1.0") {
+    title = "HTTP/1.0";
   }
 
   chrome.browserAction.setIcon({ path: "icons/" + icon + ".png", tabId });
