404c062bd5deb773070b9a7521d24ff3f3c34a96
UnknownErrror <herobrine135111@gmail.com>
2021-12-01 06:03:26 +0300

    feat: upgrade protocols detection & add SPDY support (#14)

--- src/background.js
+++ src/background.js
@@ -2,25 +2,32 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   const tabId = sender.tab.id;
   // console.log(message, tabId)
   let title = message;
-  let icon;
+  let icon = "h1";
 
-  // h2 and hq is introduced from this post:
-  // https://developers.google.com/web/updates/2017/12/chrome-loadtimes-deprecated
-  // But the latest Chrome (68) seems use the value like this:
-  // http/2+quic/43
+  // h2 and hq is introduced from this post: "https://developers.google.com/web/updates/2017/12/chrome-loadtimes-deprecated"
+  // But the latest Chrome (68) seems use the value like this: "http/2+quic/43"
   // Handle both cases for compatible
-  if (message === "h2") {
-    title = "HTTP/2";
-    icon = "h2";
-  } else if (message === "hq") {
+  
+  // Some of this taken from "https://www.iana.org/assignments/tls-extensiontype-values/tls-extensiontype-values.xhtml"
+  if (message === "hq") {
     title = "HTTP/2 + QUIC";
     icon = "hq";
   } else if (message.startsWith("h3")) {
+    title = "HTTP/3";
     icon = "h3";
+  } else if (message.startsWith("h2")) { // "h2" is "HTTP/2 over TLS", "h2c" is "HTTP/2 over TCP"
+    title = "HTTP/2";
+    icon = "h2";
   } else if (message.includes("quic")) {
+    title = "QUIC";
     icon = "hq";
-  } else {
-    icon = "h1";
+  } else if (message.startsWith("spdy")) { // "spdy/1", "spdy/2", "spdy/3"
+    title = "SPDY";
+    icon = "spdy";
+  } else if (message === "http/1.1") {
+    title = "HTTP/1.1";
+  } else if (message === "http/1.0") {
+    title = "HTTP/1.0";
   }
 
   chrome.browserAction.setIcon({ path: "icons/" + icon + ".png", tabId });
